6234cad989d679aec90d5bba753c3d30011b62d8
Waldo-Blom <waldoblom2004@gmail.com>
2025-07-29 11:23:53 +0200

    created index.ejs for login (instead of index.html)

--- server/app.js
+++ server/app.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const translateRoutes = require('.\Backend_stuff\routes\translateRoute.js');
+// const translateRoutes = require('../Backend_stuff/routes/translateRoute');
 
 app.use(express.json());
-app.use('/translate', translateRoutes);
+// app.use('/translate', translateRoutes);
 
-app.listen(8000, () => console.log("Server running on port 8000"));
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../client/views'));
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Route to render index.ejs
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// Start the server
+const port = 8000;
+app.listen(port, () => console.log(`http://localhost:${port}`));
