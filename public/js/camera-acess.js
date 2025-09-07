147fa9c77c34cd7b9c52c7dada9810374dce0f8c
Waldo-Blom <waldoblom2004@gmail.com>
2025-09-07 16:03:35 +0200

    Fixed the camera expand btn

--- public/js/camera-acess.js
+++ public/js/camera-acess.js
@@ -156,15 +156,29 @@
                 console.log(`Switched to ${mode} detection mode`);
             }
 
-            toggleExpanded() {
+           toggleExpanded() {
                 this.isExpanded = !this.isExpanded;
                 
                 if (this.isExpanded) {
                     this.cameraSection.classList.add('expanded');
                     document.body.style.overflow = 'hidden';
+                    // Update expand button icon to show "contract" symbol
+                    document.getElementById('expandBtn').innerHTML = `
+                        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                            <path d="M8 3v3a2 2 0 0 1-2 2H3m18 0h-3a2 2 0 0 1-2-2V3m0 18v-3a2 2 0 0 1 2-2h3M3 16h3a2 2 0 0 1 2 2v3"/>
+                        </svg>
+                    `;
                 } else {
                     this.cameraSection.classList.remove('expanded');
                     document.body.style.overflow = '';
+                    // Revert to expand icon
+                    document.getElementById('expandBtn').innerHTML = `
+                        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                            <path d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 
+                                    18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 
+                                    2h3"/>
+                        </svg>
+                    `;
                 }
                 
                 // Resize canvas after transition
