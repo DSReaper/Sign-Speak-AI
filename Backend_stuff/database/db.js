8a3564e8c8454b22c1488e41330f5faff8ca5905
Drago117-code <600613@student.belgiumcampus.ac.za>
2025-09-10 19:17:58 +0200

    Update db.js

--- Backend_stuff/database/db.js
+++ Backend_stuff/database/db.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const uri = process.env.MONGODB_URI || 'mongodb+srv://Zoe:kaas@mainsign.7aqed10.mongodb.net/?retryWrites=true&w=majority';
+const uri = process.env.MONGODB_URI || 'mongodb+srv://Zoe:kaas@mainsign.7aqed10.mongodb.net/';
 
 const connectDB = async () => {
   try {
